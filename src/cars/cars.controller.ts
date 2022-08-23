9e512f62a4d0afa4e72a2af97cd9cb76d3b7d53c
Tomas Ferreras <hellotomasdev@gmail.com>
2022-08-23 13:22:11 -0300

    feat: implement car by id method

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
+  private cars = ['Dodge', 'Jeep', 'Ford'];
+
   @Get()
   getAllCars() {
-    return ['Dodge', 'Jeep', 'Ford'];
+    return this.cars;
+  }
+
+  @Get(':id')
+  getCarById(@Param('id') id: string) {
+    return { car: this.cars[id] };
   }
 }
