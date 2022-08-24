d8723c374735642c6ab1a0f5c1df26c7277e2029
Tomas Ferreras <hellotomasdev@gmail.com>
2022-08-24 11:48:11 -0300

    feat: implement class validators with global pipes

--- src/cars/dto/create-car.dto.ts
+++ src/cars/dto/create-car.dto.ts
@@ -1,4 +1,10 @@
+import { IsBoolean, isString, IsString, MinLength } from 'class-validator';
+
 export class CreateCarDto {
+  @IsString()
   readonly brand: string;
+
+  @IsString()
+  @MinLength(1)
   readonly model: string;
 }
