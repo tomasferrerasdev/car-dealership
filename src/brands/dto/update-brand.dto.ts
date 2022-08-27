07ef5c7029d24e09b9bad45a52882e47f0123db4
Tomas Ferreras <hellotomasdev@gmail.com>
2022-08-27 13:02:24 -0300

    feat: add brand crud with nest cli and config each endpoint

--- src/brands/dto/update-brand.dto.ts
+++ src/brands/dto/update-brand.dto.ts
@@ -1,4 +1,14 @@
-import { PartialType } from '@nestjs/mapped-types';
+/*import { PartialType } from '@nestjs/mapped-types';
 import { CreateBrandDto } from './create-brand.dto';
+import { UpdateCarDto } from '../../cars/dto/update-car.dto';
 
 export class UpdateBrandDto extends PartialType(CreateBrandDto) {}
+*/
+
+import { IsString, MinLength } from 'class-validator';
+
+export class UpdateBrandDto {
+  @IsString()
+  @MinLength(1)
+  name: string;
+}
